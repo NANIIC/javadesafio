88d2acc6c56b23b9bce6b3f848f284fbc4c6d5fd
anahi <luna.calderon@mi.unc.edu.ar>
2024-09-18 23:18:04 -0300

    Inicializando los valores

---
+++ src/desafio.java
@@ -0,0 +1,16 @@
+public class desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoCuenta = "Corriente";
+        double saldo = 1599.99;
+
+
+        System.out.println("*****************");
+        //voy a agregar una barra invertida + n para que no quede pegado a el primer print
+        System.out.println("\nNombre del cliente: " + nombre );
+        System.out.println("Su tipo de cuenta es: " + tipoCuenta);
+        System.out.println("El Saldo es: $" + saldo);
+
+    }
+
+}
